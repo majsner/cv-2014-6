50e80489422348c8b47e7c18b682d8252b27b318
Jan <jan.masner@izun.eu>
2014-11-03 13:26:43 +0100

    Výchozí projekt

---
+++ hotel/src/main/java/pef/kit/hotel/HotelActivity.java
@@ -0,0 +1,117 @@
+package pef.kit.hotel;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Patterns;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class HotelActivity extends ActionBarActivity implements FormFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotel);
+        FormFragment formFragment = FormFragment.newInstance();
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, formFragment)
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_hotel, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showToast(CharSequence text) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public final static boolean isValidEmail(CharSequence target) {
+        if (target == null) {
+            return false;
+        } else {
+            return Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
+    }
+    public final static boolean isValidPhone(CharSequence target) {
+        if (target == null) {
+            return false;
+        } else {
+            return Patterns.PHONE.matcher(target).matches();
+        }
+    }
+
+    private void loopLayoutReset(LinearLayout ll){
+        int count = ll.getChildCount();
+        for (int i = 0;i <= count;i++){
+            View v = ll.getChildAt(i);
+            if (v instanceof LinearLayout) loopLayoutReset((LinearLayout) v);
+            if (v != null && v instanceof EditText) {
+                clearText(v);
+            }
+        }
+    }
+
+    private void clearText(View v){
+        ((TextView) v).setText("");
+    }
+
+    @Override
+    public void sendButton() {
+        if (((EditText) findViewById(R.id.name)).getText() == null || ((EditText) findViewById(R.id.name)).getText().equals("")) {
+            showToast(getString(R.string.nameValidation));
+            return;
+        }
+        if (!isValidPhone(((EditText) findViewById(R.id.phone)).getText())) {
+            showToast(getString(R.string.phoneValidation));
+            return;
+        }
+        if (!isValidEmail(((EditText)findViewById(R.id.email)).getText())) {
+            showToast(getString(R.string.emailValidation));
+            return;
+        }
+    }
+
+    @Override
+    public void resetButton(){
+        loopLayoutReset((LinearLayout) findViewById(R.id.layout));
+        NumberPicker numberPicker = (NumberPicker) findViewById(R.id.days);
+        numberPicker.setValue(1);
+        ((CheckBox) findViewById(R.id.breakfast)).setChecked(false);
+    }
+}
